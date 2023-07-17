0c2c555b0c00e4ea0d4e662bb1e592b46ec0368c
Rajalakshmi-123 <139631302+Rajalakshmi-123@users.noreply.github.com>
2023-07-17 12:31:38 +0200

    Add files via upload

---
+++ Browser1.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Browser1 {
+
+	public static void main(String[] args) {
+		
+		
+		Chrome obj=new Chrome();
+				
+				obj.getname();
+		obj.printname();
+		
+
+	}
+
+}
---
+++ Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+	
+
+		public void getname() {
+			System.out.println("This is my google chrome");
+			
+		}
+	public void printname() {
+		System.out.println("This is my gmail");
+	}
+
+
+	public static void main(String[] args) {
+		
+		Chrome c=new Chrome();
+				
+				c.getname();
+		c.printname();
+		
+	}
+
+}
