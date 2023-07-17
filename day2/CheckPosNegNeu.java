5620ac9ae5883881e1ef01d5110b0b103a37232e
Rajalakshmi-123 <139631302+Rajalakshmi-123@users.noreply.github.com>
2023-07-17 19:35:33 +0200

    Add files via upload

---
+++ day2/CheckPosNegNeu.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class CheckPosNegNeu {
+
+	public static void main(String[] args) {
+		int num=35;
+		
+		if(num>1) {
+			System.out.println("It is a positive number");
+		}
+		else if(num==0) {
+			
+			System.out.println("It is neutral");
+		}
+		else {
+			System.out.println("It is a negative number");
+		}
+		
+	}
+
+}
