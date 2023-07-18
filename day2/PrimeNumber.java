0a11849f0a11e80a6553be04e610fb07e075802b
Rajalakshmi-123 <139631302+Rajalakshmi-123@users.noreply.github.com>
2023-07-18 18:04:02 +0200

    Add files via upload

---
+++ day2/PrimeNumber.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int i =13;
+		
+boolean flag= true;
+
+for (int j = 2; j <=i/2; j++) {
+	
+	if (i%j!=0) {
+		System.out.println("13 is a Prime number "+ flag);
+		break;
+		
+
+}
+	}
+
+	}
+}
