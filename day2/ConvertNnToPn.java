5c9cfeb3377f7c4bf5870de686bca88bfcf1c133
Rajalakshmi-123 <139631302+Rajalakshmi-123@users.noreply.github.com>
2023-07-18 18:35:29 +0200

    Add files via upload

---
+++ day2/ConvertNnToPn.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class ConvertNnToPn {
+
+	public static void main(String[] args) {
+		
+		int num=-40;
+		
+		if(num<0) {
+			System.out.println("The number -40 is converted to 40");
+		}
+		
+		int num1=40;
+		
+		if(num1<0) {
+			System.out.println("The number -40 is converted to 40");
+		}
+			else {
+				System.out.println("The number cannot be converted");
+			}
+		}
+
+	}
